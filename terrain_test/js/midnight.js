005a8ce40ca9d3990fb499571a8df22e264c46df
Channing <channing93@gmail.com>
2015-05-08 19:19:39 -0400

    clean unused code

--- terrain_test/js/midnight.js
+++ terrain_test/js/midnight.js
@@ -230,10 +230,6 @@ function addCell(iOff, jOff) {
         vertices[j + 1] = data[i] * Options.heightMultiplier;
     }
 
-    //var texture = new THREE.Texture(mountainTexture);
-    //var texture = new THREE.Texture(generateTexture(data, Options.worldWidth, Options.worldDepth), THREE.UVMapping, THREE.ClampToEdgeWrapping, THREE.ClampToEdgeWrapping);
-    //texture.needsUpdate = true;
-
     geometry.computeVertexNormals();
 
     var lambertShader = THREE.ShaderLib['lambert'];
@@ -438,73 +434,6 @@ function generateHeight(width, height, iOff, jOff) {
     return data;
 }
 
-function generateTexture(data, width, height) {
-    var canvas, canvasScaled, context, image, imageData,
-    level, diff, vector3, sun, shade;
-
-    vector3 = new THREE.Vector3(0, 0, 0);
-
-    sun = new THREE.Vector3(1, 1, 1);
-    sun.normalize();
-
-    canvas = document.createElement('canvas');
-    canvas.width = width;
-    canvas.height = height;
-
-    context = canvas.getContext('2d');
-    context.fillStyle = '#000';
-    context.fillRect(0, 0, width, height);
-
-    image = context.getImageData(0, 0, canvas.width, canvas.height);
-    imageData = image.data;
-
-    for (var i = 0, j = 0, l = imageData.length; i < l; i += 4, j ++) {
-        xMin = Utils.clamp(j - 2, 0, data.length - 1);
-        xMax = Utils.clamp(j + 2, 0, data.length - 1);
-        zMin = Utils.clamp(j - width * 2, 0, data.length - 1);
-        zMax = Utils.clamp(j + width * 2, 0, data.length - 1);
-
-        vector3.x = data[xMin] - data[xMax];
-        vector3.y = 2;
-        vector3.z = data[zMin] - data[zMax];
-        vector3.normalize();
-
-        shade = vector3.dot(sun);
-        shade = Utils.quantize(shade, 0.4);
-
-        imageData[i] = (96 + shade * 128) * (0.5 + data[j] * 0.007);
-        imageData[i + 1] = (32 + shade * 96) * (0.5 + data[j] * 0.007);
-        imageData[i + 2] = (shade * 96) * (0.5 + data[j] * 0.007);
-
-    }
-
-    context.putImageData(image, 0, 0);
-
-    // Scaled 4x
-    canvasScaled = document.createElement('canvas');
-    canvasScaled.width = width * 4;
-    canvasScaled.height = height * 4;
-
-    context = canvasScaled.getContext('2d');
-    context.scale(4, 4);
-    context.drawImage(canvas, 0, 0);
-
-    image = context.getImageData(0, 0, canvasScaled.width, canvasScaled.height);
-    imageData = image.data;
-
-    for (var i = 0, l = imageData.length; i < l; i += 4) {
-        var v = ~~ (Math.random() * 5);
-
-        imageData[i] += v;
-        imageData[i + 1] += v;
-        imageData[i + 2] += v;
-    }
-
-    context.putImageData(image, 0, 0);
-
-    return canvasScaled;
-}
-
 function loadCells() {
     function posToCell(p) {
         return Math.floor((p + Options.cellSize / 2) / Options.cellSize);
@@ -608,7 +537,6 @@ function animate() {
     if (dead) return;
     requestAnimationFrame(animate);
 
-    var texture = new THREE.Texture(generateTexture(data, Options.worldWidth, Options.worldDepth), THREE.UVMapping, THREE.ClampToEdgeWrapping, THREE.ClampToEdgeWrapping);
     frame++;
     render();
     stats.update();
