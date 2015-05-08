249a17fceb9ca80bd7a78b4f557793f1eb1b3846
Channing <channing93@gmail.com>
2015-05-07 22:35:19 -0400

    standardize spacing

--- terrain_test/js/midnight.js
+++ terrain_test/js/midnight.js
@@ -36,14 +36,14 @@ init();
 animate();
 
 function init() {
-    var container = document.getElementById( 'container' );
+    var container = document.getElementById('container');
 
-    camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 1, 20000 );
+    camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 1, 20000);
 
     scene = new THREE.Scene();
-    scene.fog = new THREE.FogExp2(Options.bgColor, 0.0004 );
+    scene.fog = new THREE.FogExp2(Options.bgColor, 0.0004);
 
-    controls = new THREE.FirstPersonControls( camera );
+    controls = new THREE.FirstPersonControls(camera);
     controls.fastSpeed = Options.fastSpeed;
     controls.slowSpeed = Options.slowSpeed;
     controls.lookSpeed = 0.2;
@@ -57,20 +57,20 @@ function init() {
     camera.position.y += 200;
 
     renderer = new THREE.WebGLRenderer();
-    renderer.setClearColor( Options.bgColor );
-    renderer.setPixelRatio( window.devicePixelRatio );
-    renderer.setSize( window.innerWidth, window.innerHeight );
+    renderer.setClearColor(Options.bgColor);
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
 
     container.innerHTML = "";
 
-    container.appendChild( renderer.domElement );
+    container.appendChild(renderer.domElement);
 
     stats = new Stats();
     stats.domElement.style.position = 'absolute';
     stats.domElement.style.top = '0px';
-    container.appendChild( stats.domElement );
+    container.appendChild(stats.domElement);
 
-    window.addEventListener( 'resize', onWindowResize, false );
+    window.addEventListener('resize', onWindowResize, false);
     sphereScene = new THREE.Object3D();
     scene.add(sphereScene);
     sphereOctree = new THREE.Octree({
@@ -95,22 +95,22 @@ function findGround(pos) {
 }
 
 function addCell(iOff, jOff) {
-    var data = generateHeight( Options.worldWidth, Options.worldDepth, iOff, jOff);
-    var geometry = new THREE.PlaneBufferGeometry( Options.cellSize, Options.cellSize, Options.worldWidth - 1, Options.worldDepth - 1 );
-    geometry.applyMatrix( new THREE.Matrix4().makeRotationX( - Math.PI / 2 ) );
+    var data = generateHeight(Options.worldWidth, Options.worldDepth, iOff, jOff);
+    var geometry = new THREE.PlaneBufferGeometry(Options.cellSize, Options.cellSize, Options.worldWidth - 1, Options.worldDepth - 1);
+    geometry.applyMatrix(new THREE.Matrix4().makeRotationX(- Math.PI / 2));
 
     var vertices = geometry.attributes.position.array;
 
-    for ( var i = 0, j = 0, l = vertices.length; i < l; i ++, j += 3 ) {
-        vertices[ j + 1 ] = data[ i ] * Options.heightMultiplier;
+    for (var i = 0, j = 0, l = vertices.length; i < l; i ++, j += 3) {
+        vertices[j + 1] = data[i] * Options.heightMultiplier;
     }
 
-    var texture = new THREE.Texture( generateTexture( data, Options.worldWidth, Options.worldDepth ), THREE.UVMapping, THREE.ClampToEdgeWrapping, THREE.ClampToEdgeWrapping );
+    var texture = new THREE.Texture(generateTexture(data, Options.worldWidth, Options.worldDepth), THREE.UVMapping, THREE.ClampToEdgeWrapping, THREE.ClampToEdgeWrapping);
     texture.needsUpdate = true;
 
-    var mesh = new THREE.Mesh( geometry, new THREE.MeshBasicMaterial( { map: texture } ) );
+    var mesh = new THREE.Mesh(geometry, new THREE.MeshBasicMaterial({ map: texture }));
 
-    var meshShadow = new THREE.Mesh( geometry, new THREE.MeshBasicMaterial( { map: texture, side:THREE.BackSide, color:0x0 } ) );
+    var meshShadow = new THREE.Mesh(geometry, new THREE.MeshBasicMaterial({ map: texture, side:THREE.BackSide, color:0x0 }));
     meshShadow.scale.multiplyScalar(1.003);
 
     var cell = new THREE.Object3D();
@@ -137,10 +137,10 @@ function addSphere() {
     var sphere = new THREE.Object3D();
 
     var geometry = new THREE.SphereGeometry(20, 32, 32);
-    var material = new THREE.MeshBasicMaterial( {color: 0xffff00} );
-    var materialOutline = new THREE.MeshBasicMaterial( {color: 0x0, side: THREE.BackSide } );
-    var sphereBody = new THREE.Mesh( geometry, material );
-    var sphereOutline = new THREE.Mesh( geometry, materialOutline );
+    var material = new THREE.MeshBasicMaterial({color: 0xffff00});
+    var materialOutline = new THREE.MeshBasicMaterial({color: 0x0, side: THREE.BackSide });
+    var sphereBody = new THREE.Mesh(geometry, material);
+    var sphereOutline = new THREE.Mesh(geometry, materialOutline);
     sphereOutline.scale.multiplyScalar(1.2);
     sphere.add(sphereBody);
     sphere.add(sphereOutline);
@@ -161,8 +161,8 @@ function addSphere() {
     return sphere;
 }
 
-function generateHeight( width, height, iOff, jOff ) {
-    var size = width * height, data = new Uint8Array( size ),
+function generateHeight(width, height, iOff, jOff) {
+    var size = width * height, data = new Uint8Array(size),
         perlin = new ImprovedNoise(), quality = 1;
     var z = Options.seed;
     var iOff = iOff || 0;
@@ -171,12 +171,12 @@ function generateHeight( width, height, iOff, jOff ) {
     iOff += globalOffset;
     jOff += globalOffset;
 
-    for ( var j = 0; j < 4; j ++ ) {
-        for ( var i = 0; i < size; i ++ ) {
-            var x = i % width, y = ~~ ( i / width );
+    for (var j = 0; j < 4; j ++) {
+        for (var i = 0; i < size; i ++) {
+            var x = i % width, y = ~~ (i / width);
             x += iOff * (width - 1);
             y += jOff * (height - 1);
-            data[ i ] += Math.abs( perlin.noise( x / quality, y / quality, z ) * quality * 1.75 );
+            data[i] += Math.abs(perlin.noise(x / quality, y / quality, z) * quality * 1.75);
         }
         quality *= 5;
 
@@ -184,68 +184,68 @@ function generateHeight( width, height, iOff, jOff ) {
     return data;
 }
 
-function generateTexture( data, width, height ) {
+function generateTexture(data, width, height) {
     var canvas, canvasScaled, context, image, imageData,
     level, diff, vector3, sun, shade;
 
-    vector3 = new THREE.Vector3( 0, 0, 0 );
+    vector3 = new THREE.Vector3(0, 0, 0);
 
-    sun = new THREE.Vector3( 1, 1, 1 );
+    sun = new THREE.Vector3(1, 1, 1);
     sun.normalize();
 
-    canvas = document.createElement( 'canvas' );
+    canvas = document.createElement('canvas');
     canvas.width = width;
     canvas.height = height;
 
-    context = canvas.getContext( '2d' );
+    context = canvas.getContext('2d');
     context.fillStyle = '#000';
-    context.fillRect( 0, 0, width, height );
+    context.fillRect(0, 0, width, height);
 
-    image = context.getImageData( 0, 0, canvas.width, canvas.height );
+    image = context.getImageData(0, 0, canvas.width, canvas.height);
     imageData = image.data;
 
-    for ( var i = 0, j = 0, l = imageData.length; i < l; i += 4, j ++ ) {
+    for (var i = 0, j = 0, l = imageData.length; i < l; i += 4, j ++) {
         xMin = Utils.clamp(j - 2, 0, data.length - 1);
         xMax = Utils.clamp(j + 2, 0, data.length - 1);
         zMin = Utils.clamp(j - width * 2, 0, data.length - 1);
         zMax = Utils.clamp(j + width * 2, 0, data.length - 1);
 
-        vector3.x = data[ xMin ] - data[ xMax ];
+        vector3.x = data[xMin] - data[xMax];
         vector3.y = 2;
-        vector3.z = data[ zMin ] - data[ zMax ];
+        vector3.z = data[zMin] - data[zMax];
         vector3.normalize();
 
-        shade = vector3.dot( sun );
+        shade = vector3.dot(sun);
         shade = Utils.quantize(shade, 0.4);
 
-        imageData[ i ] = ( 96 + shade * 128 ) * ( 0.5 + data[ j ] * 0.007 );
-        imageData[ i + 1 ] = ( 32 + shade * 96 ) * ( 0.5 + data[ j ] * 0.007 );
-        imageData[ i + 2 ] = ( shade * 96 ) * ( 0.5 + data[ j ] * 0.007 );
+        imageData[i] = (96 + shade * 128) * (0.5 + data[j] * 0.007);
+        imageData[i + 1] = (32 + shade * 96) * (0.5 + data[j] * 0.007);
+        imageData[i + 2] = (shade * 96) * (0.5 + data[j] * 0.007);
     }
 
-    context.putImageData( image, 0, 0 );
+    context.putImageData(image, 0, 0);
 
     // Scaled 4x
-    canvasScaled = document.createElement( 'canvas' );
+    canvasScaled = document.createElement('canvas');
     canvasScaled.width = width * 4;
     canvasScaled.height = height * 4;
 
-    context = canvasScaled.getContext( '2d' );
-    context.scale( 4, 4 );
-    context.drawImage( canvas, 0, 0 );
+    context = canvasScaled.getContext('2d');
+    context.scale(4, 4);
+    context.drawImage(canvas, 0, 0);
 
-    image = context.getImageData( 0, 0, canvasScaled.width, canvasScaled.height );
+    image = context.getImageData(0, 0, canvasScaled.width, canvasScaled.height);
     imageData = image.data;
 
-    for ( var i = 0, l = imageData.length; i < l; i += 4 ) {
-        var v = ~~ ( Math.random() * 5 );
+    for (var i = 0, l = imageData.length; i < l; i += 4) {
+        var v = ~~ (Math.random() * 5);
 
-        imageData[ i ] += v;
-        imageData[ i + 1 ] += v;
-        imageData[ i + 2 ] += v;
+        imageData[i] += v;
+        imageData[i + 1] += v;
+        imageData[i + 2] += v;
     }
 
-    context.putImageData( image, 0, 0 );
+    context.putImageData(image, 0, 0);
 
     return canvasScaled;
 }
@@ -308,8 +308,8 @@ function checkSphereCollision() {
 }
 
 function render() {
-    controls.update( clock.getDelta() );
-    renderer.render( scene, camera );
+    controls.update(clock.getDelta());
+    renderer.render(scene, camera);
     sphereOctree.update();
     loadCells();
     checkCollision();
@@ -319,7 +319,7 @@ function render() {
 
 function animate() {
     if (dead) return;
-    requestAnimationFrame( animate );
+    requestAnimationFrame(animate);
     frame++;
     render();
     stats.update();
@@ -328,6 +328,6 @@ function animate() {
 function onWindowResize() {
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
-    renderer.setSize( window.innerWidth, window.innerHeight );
+    renderer.setSize(window.innerWidth, window.innerHeight);
     controls.handleResize();
 }
